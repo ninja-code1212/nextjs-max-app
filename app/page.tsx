903b985981774d4011de110ecde976c15b6c2f37
Maxim T <maxtit1212@gmail.com>
2023-07-29 21:18:32 +0300

    first-commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,10 @@ export default function Home() {
   return (
     <main>
       <div className="container mx-auto mt-8">
-        <div className="rounded-lg bg-white shadow-2xl px-4 py-4">
-          Контейнер
+        <div className="rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 shadow-2xl px-4 py-4">
+          <h1 className="mainpage-title ">Добро пожаловать на сайт Максима</h1>
         </div>
+        <title>Максим</title>
       </div>
     </main>
   )
