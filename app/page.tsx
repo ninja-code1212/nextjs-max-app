b5e74fc7dbad8cd5ea6d1c203cd63133d505ef75
Maxim T <maxtit1212@gmail.com>
2023-07-30 11:56:44 +0300

    First-Commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,11 @@ export default function Home() {
   return (
     <main>
       <div className="container mx-auto mt-8">
-        <div className="rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 shadow-2xl px-4 py-4">
-          <h1 className="mainpage-title ">Добро пожаловать на сайт Максима</h1>
+        <div className="bg-gradient-to-r from-green-400 via-violet-600 to-pink-500 shadow-2xl px-4 py-4">
+          <p className="mainpage-title underline ...">Добро пожаловать на сайт Максима</p>
+          <button className="Button outline outline-offset-2 outline-blue-500">Docs</button>
+          <title>Максим</title>
         </div>
-        <title>Максим</title>
       </div>
     </main>
   )
