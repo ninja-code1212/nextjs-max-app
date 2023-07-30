53ecc9e73bb91acfade82033ebe5544ab6b36d96
Maxim T <maxtit1212@gmail.com>
2023-07-30 21:28:36 +0300

    Five-commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="container mx-auto mt-8">
         <div className="bg-gradient-to-r from-green-400 via-violet-600 to-pink-500 shadow-2xl px-4 py-4">
           <p className="mainpage-title underline ...">Добро пожаловать на сайт Максима</p>
-          <button className="Button outline outline-offset-2 outline-blue-500">Docs</button>
+          <a href="https://github.com/ninja-code1212" className="my-button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">button</a>
           <title>Максим</title>
         </div>
       </div>
