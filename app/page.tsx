9cf1a8d5dbbf09a7b8eee9d0c912d3eac4547d9d
Maxim T <maxtit1212@gmail.com>
2023-07-31 12:40:19 +0300

    #

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
         <div className="bg-gradient-to-r from-green-400 via-violet-600 to-pink-500 shadow-2xl px-4 py-4">
           <p className="mainpage-title">Добро пожаловать на сайт Максима</p>
           <a href="https://github.com/ninja-code1212" className="my-button bg-blue-500 hover:bg-blue-700 text-red font-impact py-2 px-4 rounded-full">Github</a>
+          <button disabled="button bg-blue-500 hover:bg-blue-700 text-red font-impact py-2 px-4 rounded-full"></button>
           <title>Максим</title>
-          <a href="#" className="my-button bg-blue-500 hover:bg-blue-700 text-red font-impact py-2 px-4 rounded-full">Docs</a>
         </div>
       </div>
     </main>
